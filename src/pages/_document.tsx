232a0d68e51348c45f249fe4a6e8efb64113f6b7
Pranav VS <pranuvs7@gmail.com>
2025-03-14 20:11:23 +0530

    fix: Added logo.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import nextConfig from "../../next.config";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta
+          name="description"
+          content="RTI-Helper. Use this free tool to fill RTI Applications."
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${nextConfig.basePath}/logo.png`}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
