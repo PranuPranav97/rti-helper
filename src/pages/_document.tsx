5a8e3c05a4f09f139dc0d65e3aa49adff89d4a6b
Pranav VS <pranuvs7@gmail.com>
2025-03-14 20:23:19 +0530

    fix: Reverted to old head code.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,21 +4,8 @@ import nextConfig from "../../next.config";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
-        <meta
-          name="description"
-          content="RTI-Helper. Use this free tool to fill RTI Applications."
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${nextConfig.basePath}logo.png`}
-        />
-      </Head>
+      <Head />
+
       <body className="antialiased">
         <Main />
         <NextScript />
