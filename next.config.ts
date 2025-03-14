be8e77c4f301a8c49a135ccfed10c514050903a5
Pranav VS <pranuvs7@gmail.com>
2025-03-14 19:37:58 +0530

    fix: patch to the next-config.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  basePath: process.env.NODE_ENV === "development" ? undefined : "/rti-helper",
 };
 
 export default nextConfig;
